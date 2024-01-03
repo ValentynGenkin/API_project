22559465eface8f65915a02d1f9ef1c082632fde
Valentyn Genkin <genkin.valentyn@gmail.com>
2024-01-03 16:53:55 +0100

    env file added

--- server/app.js
+++ server/app.js
@@ -5,4 +5,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api/auth');
+app.use('/api/collection');
+app.use('/api/admin');
+
 export default app;
