f6b8a0e91c9451905ea4e2ae7366840176e32cf4
Valentyn Genkin <genkin.valentyn@gmail.com>
2024-01-04 15:29:50 +0100

    login controller added

---
+++ server/controllers/login.js
@@ -0,0 +1,28 @@
+import bcrypt from 'bcryptjs';
+import User from '../db/models/userModel.js';
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const comparePassword = (password, hashedPassword) => {
+    return bcrypt.compareSync(password, comparePassword);
+  };
+
+  if (!email || !password) {
+    return res.status(400).json({
+      msg: 'Check email and/or password',
+    });
+  }
+
+  const user = await User.findOne({ email: email }).select('+password');
+  if (!user || !comparePassword(password, user.password)) {
+    return res.status(401).json({
+      msg: 'Invalid username or password.',
+    });
+  }
+
+  const userObject = user.toObject();
+  delete userObject.password;
+
+  return res.status(200).json({ userObject });
+};
