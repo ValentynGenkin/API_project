1bb3b2f29a22362dc3a0d35081d6c2a700ee88a2
Valentyn Genkin <genkin.valentyn@gmail.com>
2024-01-05 12:46:14 +0100

    logout endpoint added

---
+++ server/controllers/logout.js
@@ -0,0 +1,20 @@
+export const logout = async (req, res) => {
+  try {
+    if (req.cookies.customer_access) {
+      return res
+        .status(200)
+        .clearCookie('customer_access')
+        .json({ msg: 'You have logged out successfully' });
+    } else {
+      return res.status(400).json({
+        msg: 'You have already logged out',
+      });
+    }
+  } catch (error) {
+    console.error('Logout error', error);
+    return res.status(500).json({
+      msg: 'Internal Server Error',
+      error: error.message,
+    });
+  }
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { createNewUser } from '../controllers/createNewUser.js';
 import { login } from '../controllers/login.js';
+import { logout } from '../controllers/logout.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/registration', createNewUser);
 authRouter.post('/login', login);
+authRouter.get('/logout', logout);
 
 export default authRouter;
