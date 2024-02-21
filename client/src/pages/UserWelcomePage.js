cbbe048cccc5e660b7aa81f0e95a2e84716b466e
Valentyn Genkin <genkin.valentyn@gmail.com>
2024-02-21 16:23:57 +0100

    auth check added to user menu page

--- client/src/pages/UserWelcomePage.js
+++ client/src/pages/UserWelcomePage.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Container from 'react-bootstrap/esm/Container';
-
+import { useNavigate } from 'react-router-dom';
 import '../styles/userWelcomePage.css';
+import { default as useFetchAuth } from '../hooks/useFetch';
 import UserWelcomeCards from '../components/UserWelcomeCards';
 import UserTextBlock from '../components/UserTextBlock';
 
 const UserWelcomePage = () => {
+  const navigation = useNavigate();
+  const [authData, , authError, authFetchData] = useFetchAuth(
+    `api/auth/authentication`,
+  );
+
+  useEffect(() => {
+    authFetchData({
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }, []);
+
+  useEffect(() => {
+    if (authError && !authError.success) {
+      navigation('/');
+    }
+  }, [authError]);
+
   return (
     <Container className="user-welcome-page-container">
-      <UserTextBlock />
-      <UserWelcomeCards />
+      {authData && (
+        <>
+          {' '}
+          <UserTextBlock />
+          <UserWelcomeCards />
+        </>
+      )}
     </Container>
   );
 };
