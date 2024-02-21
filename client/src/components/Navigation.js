d20b324d63c93b42b7eeb8c5b61c439e86780ff5
Valentyn Genkin <genkin.valentyn@gmail.com>
2024-02-21 16:17:44 +0100

    logout connected to API

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,34 +1,80 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Container from 'react-bootstrap/esm/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import '../styles/navigation.css';
+import useFetch from '../hooks/useFetch';
+import Spinner from 'react-bootstrap/esm/Spinner';
 
 const Navigation = () => {
+  const [data, isLoading, error, fetchData] = useFetch(`api/auth/logout`);
+
+  const navigation = useNavigate();
+
+  const logout = () => {
+    fetchData({
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  };
+
+  useEffect(() => {
+    if (data && data.success) {
+      navigation('/');
+    }
+    console.log(data);
+  }, [data]);
+
   return (
-    <Navbar data-bs-theme="light" className="navigation">
-      <Container>
-        <Navbar.Brand as={Link} to={'/'}>
-          Navbar
-        </Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link as={Link} to={'/user-menu'}>
-            Home
+    <>
+      <Navbar data-bs-theme="light" className="navigation">
+        <Container>
+          <Navbar.Brand as={Link} to={'/'}>
+            Navbar
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to={'/user-menu'}>
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to={'schema'}>
+              Schema
+            </Nav.Link>
+            <Nav.Link as={Link} to={'endpoint'}>
+              Endpoints
+            </Nav.Link>
+            <Nav.Link as={Link}>Statistics</Nav.Link>
+            <Nav.Link as={Link}>Account</Nav.Link>
+          </Nav>
+          <Nav.Link
+            onClick={() => {
+              logout();
+            }}
+          >
+            {isLoading ? (
+              <>
+                <Spinner
+                  as="span"
+                  animation="border"
+                  size="sm"
+                  role="status"
+                  aria-hidden="true"
+                  className="loading-spinner"
+                />
+                <span className="visually-hidden">Loading...</span>
+              </>
+            ) : (
+              'Sign out'
+            )}
           </Nav.Link>
-          <Nav.Link as={Link} to={'schema'}>
-            Schema
-          </Nav.Link>
-          <Nav.Link as={Link} to={'endpoint'}>
-            Endpoints
-          </Nav.Link>
-          <Nav.Link as={Link}>Statistics</Nav.Link>
-          <Nav.Link as={Link}>Account</Nav.Link>
-        </Nav>
-        <Nav.Link>Sign out</Nav.Link>
-      </Container>
-    </Navbar>
+        </Container>
+      </Navbar>
+      {error && <p>{error.msg}</p>}
+    </>
   );
 };
 
--- server/controllers/auth/logout.js
+++ server/controllers/auth/logout.js
@@ -1,12 +1,13 @@
 export const logout = async (req, res) => {
   try {
     if (req.cookies.customer_access) {
-      return res
-        .status(200)
-        .clearCookie('customer_access')
-        .json({ msg: 'You have logged out successfully' });
+      return res.status(200).clearCookie('customer_access').json({
+        success: true,
+        msg: 'You have logged out successfully',
+      });
     } else {
       return res.status(400).json({
+        success: false,
         msg: 'You have already logged out',
       });
     }
